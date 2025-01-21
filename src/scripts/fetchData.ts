544f8be5fa7fedd8a23a5d5952b734965987cf1e
Kuby11 <saab112468@gmail.com>
2025-01-21 06:52:23 +0400

    fixed scss code and adaptability

--- src/scripts/fetchData.ts
+++ src/scripts/fetchData.ts
@@ -1,6 +1,6 @@
 export async function fetchData(city: string){
 	const API_KEY = import.meta.env.VITE_API_KEY;
-	const API_URL: string = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+	const API_URL: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
 	const response = await fetch(API_URL);
 
@@ -8,8 +8,6 @@ export async function fetchData(city: string){
 
 	if(!response){
 		throw new Error('could not fetch weather data')
-	}
-	console.log(data);
-	
+	}	
 	return await data
 }
